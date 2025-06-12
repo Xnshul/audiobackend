8b08567a9063f762a2f73c471269aa1993beb7db
Anshul koshta <ANSHULKOSHTA621@GMAIL.COM>
2025-06-12 20:19:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,24 +1,20 @@
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const path = require('path');
 require('dotenv').config();
 
 const audioRoutes = require('./routes/audioRoutes');
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://audiobackend.onrender.com' 
-}));
+app.use(cors());
 app.use(express.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/audio', audioRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     app.listen(process.env.PORT || 5000, () =>
-      console.log('✅ Server + MongoDB running'));
+      console.log('Server + MongoDB running'));
   })
-  .catch(err => console.error('❌ MongoDB error:', err));
-
+  .catch(err => console.error(' MongoDB error:', err));
