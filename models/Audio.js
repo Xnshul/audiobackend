f09627d9a989665a3ebaf540be3e2c3ed6df94a8
Anshul koshta <ANSHULKOSHTA621@GMAIL.COM>
2025-06-12 20:21:07 +0530

    Update Audio.js

--- models/Audio.js
+++ models/Audio.js
@@ -1,9 +1,12 @@
 const mongoose = require('mongoose');
 
 const audioSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  filename: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now },
+  title: String,
+  filePath: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 module.exports = mongoose.model('Audio', audioSchema);
